0b6088289a12fb601f73d1bb74635516c4a5a016
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2025-02-04 20:40:45 -0800

    Globals

--- R/globals.R
+++ R/globals.R
@@ -3,6 +3,12 @@ utils::globalVariables(
     "Active", "Catch", "CatchTons", "Date", "DisposalCode", "GearCode", "Group",
     "LocationCode", "LocationName", "ProductLanded", "Region", "RegionName",
     "SAR", "Season", "Section", "Source", "StatArea", "Year", "conv_factors",
-    "sum_na", "Area", "Gear", "Period", "Stock", "Type", "Value"
+    "sum_na", "Area", "Gear", "Period", "Stock", "Type", "Value", "Age",
+    "DualAge", "Fish", "GonadLength", "GonadWeight", "Latitude", "Length",
+    "Longitude", "MaturityCode", "Month", "Number", "Representative",
+    "Representative_Set", "SampWt", "Sample", "Set_Latitude", "Set_Longitude",
+    "Sex", "SourceCode", "Weight", "across", "age", "dual_age", "gear_code",
+    "gonad_len", "gonad_wgt", "isamp", "len", "loc_code", "mat_code", "month",
+    "na.omit", "season", "sex_alpha", "source_code", "wgt"
   )
 )
