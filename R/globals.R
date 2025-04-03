1af6da0c3fe2ea985755044de25ff919e4bb527a
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2025-04-03 10:33:18 -0700

    Spelling and globals

--- R/globals.R
+++ R/globals.R
@@ -1,16 +1,17 @@
 utils::globalVariables(
   names = c(
-    "Active", "Catch", "CatchTons", "Date", "DisposalCode", "GearCode", "Group",
-    "LocationCode", "LocationName", "ProductLanded", "Region", "RegionName",
-    "SAR", "Season", "Section", "Source", "StatArea", "Year", "conv_factors",
-    "sum_na", "Area", "Gear", "Period", "Stock", "Type", "Value", "Age",
-    "DualAge", "Fish", "GonadLength", "GonadWeight", "Latitude", "Length",
-    "Longitude", "MaturityCode", "Month", "Number", "Representative",
-    "Representative_Set", "SampWt", "Sample", "Set_Latitude", "Set_Longitude",
-    "Sex", "SourceCode", "Weight", "across", "age", "dual_age", "gear_code",
-    "gonad_len", "gonad_wgt", "isamp", "len", "loc_code", "mat_code", "month",
-    "na.omit", "season", "sex_alpha", "source_code", "wgt", "Depth", "Dive",
-    "End", "Macro", "MacroLyrs", "MacroSI", "Method", "SpawnNumber", "Start",
+    "Active", "Catch", "CatchTons", "codes_group", "database_info", "Date",
+    "DisposalCode", "GearCode", "geometry", "Group", "LocationCode",
+    "LocationName", "ProductLanded", "Region", "RegionName", "SAR", "Season",
+    "Section", "Source", "StatArea", "Year", "conv_factors", "sum_na", "Area",
+    "Gear", "Period", "Stock", "Type", "Value", "Age", "DualAge", "Fish",
+    "GonadLength", "GonadWeight", "Latitude", "Length", "Longitude",
+    "MaturityCode", "Month", "Number", "Representative", "Representative_Set",
+    "SampWt", "Sample", "Set_Latitude", "Set_Longitude", "Sex", "SourceCode",
+    "Weight", "across", "age", "dual_age", "gear_code", "gonad_len",
+    "gonad_wgt", "isamp", "len", "loc_code", "mat_code", "month", "na.omit",
+    "season", "sex_alpha", "source_code", "wgt", "Depth", "Dive", "End",
+    "Macro", "MacroLyrs", "MacroSI", "Method", "pars", "SpawnNumber", "Start",
     "SurfLyrs", "SurfSI", "Surface", "Survey", "Under", "UnderLyrs", "UnderSI",
     "Width"
   )
