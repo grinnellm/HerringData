740ac55e1a2ce174696313a94e9b61f48fa3d3d2
grinnellm <matthew.grinnell@dfo-mpo.gc.ca>
2026-05-06 13:14:22 -0700

    Clean up tests

--- make.R
+++ make.R
@@ -7,8 +7,9 @@ require(HerringData)
 source(file = here::here("data-raw", "pars.R"))
 
 # Compile the supporting documents
-devtools::build_manual(path = here::here("doc"))
-devtools::build_vignettes(pkg = ".")
+devtools::build_manual(path = here::here("docs"))
+pkgdown::build_articles()
 
 # Good practice (takes a while; restart R and require `HerringData` first)
+require(HerringData)
 goodpractice::gp(path = ".")
