8234351571ee20b79b750ebd390d60ea6c871764
grinnellm <matthew.grinnell@dfo-mpo.gc.ca>
2026-05-11 09:07:39 -0700

    Add files for data summary reports

--- R/HerringData.R
+++ R/HerringData.R
@@ -2,8 +2,9 @@
 #'
 #' `r read.dcf(file = "DESCRIPTION", fields = "Description")`
 #'
-#' The HerringData package provides data, as well as four families of
-#' functions: loaders, SISCAH, utility, and check.
+#' The HerringData package provides data, as well as the following families of
+#' functions: loaders, SISCAH, calculations, tables, figures, output, utility,
+#' and check.
 #'
 #' @section Data: The data are disposal codes (\code{\link{codes_disposal}}),
 #'   gear codes (\code{\link{codes_gear}}), group codes
@@ -17,6 +18,14 @@
 #'
 #' @section SISCAH functions: The SISCAH functions are \code{\link{siscah_bio}},
 #'   \code{\link{siscah_catch}}, and \code{\link{siscah_spawn}}.
+#'
+#' @section Calculations functions: The calculation functions are:
+#'
+#' @section Tables functions: The table functions are:
+#'
+#' @section Figures functions: The figure functions are:
+#'
+#' @section Output functions: The output functions are:
 #'
 #' @section Utility functions: The utility function are
 #'   \code{\link{mean_na_roll}}, \code{\link{season_to_year}},
--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # HerringData
 
-Load and wrangle Pacific Herring data.
-Input and output.
+Load Pacific Herring (*Clupea pallasii*) data from databases and
+wrangle for data summary reports and input to statistical catch-age models.
+Data includes biological (number- and weight-at-age), catch, and spawn
+index data.
+
+## Data summary reports
+
+Tables, figures, and other content
+for annual [data summary reports](https://github.com/grinnellm/Reports).
+See the [vignette](xxx) for an introduction to the data summaries.
+
+## Input files for stock assessment
+
+Spatially integrated statistical catch-at-age herring
+([SISCAH](https://www.publications.gc.ca/site/eng/9.959363/publication.html))
+input files:
+biological (number- and weight-at-age),
+catch, and
+spawn index data.
+See the [vignette](xxx) for an introduction to the SISCAH input data.
+
+<!-- TODO: Make and publish the HTML files (vignettes) on GitHub so that the -->
+<!-- README file refers to them -->
